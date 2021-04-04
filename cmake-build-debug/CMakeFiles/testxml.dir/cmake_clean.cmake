527ee2fe70d5930c8fa89b2c3fd9747ab13d455b
罗林旺 <lzerone@163.com>
2021-04-04 11:02:21 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/testxml.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testxml.dir/main.cpp.obj"
+  "libtestxml.dll.a"
+  "testxml.exe"
+  "testxml.exe.manifest"
+  "testxml.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testxml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
